3e13fa8935c47b8e98b8a4bc319324f81f9bb8f6
Dante <delfavero.dante@volta.ts.it>
2022-10-02 22:43:44 +0200

    Ricambio e Stato record

--- src/it/mrstark/garage/entities/Ricambio.java
+++ src/it/mrstark/garage/entities/Ricambio.java
@@ -1,11 +1,23 @@
 package it.mrstark.garage.entities;
 
+import java.time.LocalDate;
+
 import it.mrstark.garage.utilities.ValoriContabili;
 
 public class Ricambio extends ElementoMagazzino {
 
 	public static float RINCARO_RICAMBI = 0.33f;
 	public static char TIPO = 'R';
+	
+	public Ricambio( String descrizione,
+		String codice,
+		LocalDate dataAcquisto,
+		LocalDate dataVendita,
+		float costo,
+		float prezzo,
+		StatoRecord stato) {
+		super(descrizione, codice, dataAcquisto, dataVendita, costo, prezzo, stato);
+	}
 	
 	@Override
 	public char getTipo() {
--- src/it/mrstark/garage/entities/StatoRecord.java
+++ src/it/mrstark/garage/entities/StatoRecord.java
@@ -4,4 +4,7 @@ public enum StatoRecord {
 	ACQUISTATO,
 	VENDUTO,
 	
+	// metto uno stato invalido
+	NESSUNO,
+	
 }
